19bd2b2230574cc2dfa7f3b0a996b87f0f38c496
Konstantinos Vasileiou <hello@vsl.io>
2024-08-10 13:20:10 +0300

    Testing animated graph fix

--- src/AnimatedLineGraph.tsx
+++ src/AnimatedLineGraph.tsx
@@ -252,6 +252,7 @@ export function AnimatedLineGraph({
 
     setCommandsChanged(commandsChanged + 1)
 
+    interpolateProgress.value = 0
     interpolateProgress.value = withSpring(1, {
       mass: 1,
       stiffness: 500,
